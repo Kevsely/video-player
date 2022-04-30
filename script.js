8313d20a960fafe8dcbd03515f82a5c2a7f1140e
Kevsely <elegis.sossou@yahoo.com>
2022-04-30 16:41:26 +0100

    Added volume control via click events

--- script.js
+++ script.js
@@ -57,7 +57,17 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
-
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth
+    //Rounding volume up or down
+    if (volume < 0.1) volume = 0
+    if (volume > 0.9) volume = 1
+    volumeBar.style.width = `${volume * 100}%`
+    video.volume = volume
+    // Change icon depending on volume
+    
+}
 
 // Change Playback Speed -------------------- //
 
@@ -72,3 +82,4 @@ video.addEventListener("click", togglePlay)
 video.addEventListener("timeupdate", updateProgress)
 video.addEventListener("canplay", updateProgress)
 progressRange.addEventListener("click", setProgress)
+volumeRange.addEventListener("click", changeVolume)
